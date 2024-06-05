390f27ef4afcbbec8db82481d56f5621671c3297
konynunez <156865892+konynunez@users.noreply.github.com>
2024-06-05 19:39:17 -0400

    citas motivacionales app

---
+++ Practice/Cursojs/citas-motivacionales/images/citas.js
@@ -0,0 +1,42 @@
+let citas = [
+  {
+      "autor": "Albert Einstein",
+      "texto": "No poseo talentos especiales, solo soy apasionado curioso."
+  },
+  
+  {
+      "autor": "Anonimo",
+      "texto": "Semanas de programacion te pueden ahorrar horas de planeacion."
+  },
+  {
+      "autor": "Amelia Earhart",
+      "texto": "Lo mas dificil es tomar la desición de actuar, el resto es simplemente tenacidad."
+  },
+
+  {
+      "autor": "Aristoteles",
+      "texto": "La calidad no es un acto, es un hábito."
+  },
+
+  {
+      "autor": "Benjamin Franklin",
+      "texto": "Dímelo y lo olvidaré, enseñamelo y lo recordaré, involúcrame y lo aprenderé."
+  },
+  {
+      "autor": "Charles R. Swindoll",
+      "texto": "La vida es 10% lo que te ocurre y 90% como reaccionas."
+  },
+  {
+      "autor": "Jane Goodall",
+      "texto": "Lo que haces hace una diferencia y debes decidir si que tipo de diferencia quieres hacer."
+  },
+  {
+      "autor": "John Muir",
+      "texto": "El poder de la imaginacion nos hace infinitos."
+  
+  },
+  {
+      "autor": "Mark Twain",
+      "texto": "Los dos dias mas importantes de tu vida son el dia que naciste y el dia que descubres por qué."
+  },
+];
